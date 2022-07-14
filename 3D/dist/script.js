acc89f7bcf369019ac76eaf23b8161e06b107d5f
Arakistech <107245718+Arakistech@users.noreply.github.com>
2022-07-14 10:32:09 +0200

    Update script.js

--- 3D/dist/script.js
+++ 3D/dist/script.js
@@ -59,6 +59,73 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiaWFyYWtpc3RhaW4iLCJhIjoiY2w1a3B3YTMxMGNmbTNjb
         
         
         
+        const geojson = {
+'type': 'FeatureCollection',
+'features': [
+{
+'type': 'Feature',
+'properties': {
+'message': 'Foo',
+'iconSize': [60, 60]
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [-66.324462, -16.024695]
+}
+},
+{
+'type': 'Feature',
+'properties': {
+'message': 'Bar',
+'iconSize': [50, 50]
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [-61.21582, -15.971891]
+}
+},
+{
+'type': 'Feature',
+'properties': {
+'message': 'Baz',
+'iconSize': [40, 40]
+},
+'geometry': {
+'type': 'Point',
+'coordinates': [-63.292236, -18.281518]
+}
+}
+]
+};
+ 
+const map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [-65.017, -16.457],
+zoom: 5
+});
+ 
+// Add markers to the map.
+for (const marker of geojson.features) {
+// Create a DOM element for each marker.
+const el = document.createElement('div');
+const width = marker.properties.iconSize[0];
+const height = marker.properties.iconSize[1];
+el.className = 'marker';
+el.style.backgroundImage = `url(https://placekitten.com/g/${width}/${height}/)`;
+el.style.width = `${width}px`;
+el.style.height = `${height}px`;
+el.style.backgroundSize = '100%';
+ 
+el.addEventListener('click', () => {
+window.alert(marker.properties.message);
+});
+ 
+// Add markers to the map.
+new mapboxgl.Marker(el)
+.setLngLat(marker.geometry.coordinates)
+.addTo(map);
+}
       
         
         
